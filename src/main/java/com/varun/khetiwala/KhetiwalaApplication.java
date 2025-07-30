ac0144083707c56cddc8429b4fc8fe08a9f09b97
hoshiyar <hoshiyar@kugelblitz.xyz>
2025-07-30 22:31:33 +0530

    first commit

---
+++ src/main/java/com/varun/khetiwala/KhetiwalaApplication.java
@@ -0,0 +1,13 @@
+package com.varun.khetiwala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhetiwalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhetiwalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varun/khetiwala/KhetiwalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.khetiwala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhetiwalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
