5caef17606464e22640b1278add81712e4f9032b
hoshiyar <hoshiyar@kugelblitz.xyz>
2025-08-10 14:01:22 +0530

    created all the repository

---
+++ src/main/java/com/varun/khetiwala/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.varun.khetiwala.repository;
+
+
+import jakarta.persistence.criteria.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+}
